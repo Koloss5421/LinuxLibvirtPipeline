2e692fcc25b2b772dba1db1c5ee481b6c497a3dc
koloss <jphardin93@gmail.com>
2021-12-17 19:04:08 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Linux Libvirt Build Pipeline
+A build pipeline tool that utilizes libvirt to run a Windows VM and build executables
---
+++ build_exe.sh
@@ -0,0 +1,203 @@
+#!/bin/bash
+
+## Config
+VIRSH_DOMAIN="win10-dev"
+SSH_NAME="dev-machine"
+DEF_MSBUILD='C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe'
+DEF_OUTPUTDIR="bin/"
+DEF_BCONFIG="Release"
+DEF_BPLAT="x64"
+## This is the path you want to replace before
+Z_PATH="/opt/"
+
+## SSH PARAMS
+SSH_TIMEOUT=5
+SSH_RETRIES=3
+
+function print_help {
+	if [[ $1 ]]; then
+		echo -e "[!] Error: $1\n"
+	fi
+	echo "################################################"
+	echo "############ Linux Libvirt Pipeline ############"
+	echo -e "################################################\n"
+	echo -e "A build pipeline tool that utilizes libvirt to run\nA Windows VM and build executables"
+	echo -e "\nRequired Parameters: "
+	echo -e "\t-b|--build\t\tSpecify the build's csproj file location"
+	echo -e "\nOptional Parameters: "
+	echo -e "\t-r|--release\t\tSet Build Configuration to 'Release'. Cannot be used with -d. (default)"
+	echo -e "\t-d|--debug\t\tSet Build Configuration to 'Debug'. Cannot be used with -r"
+	echo -e "\t-p|--platform\t\tSet Build Platform.\n\t\t\t\tExpected Values: x86 | x64 (default) | AnyCpu"
+	echo -e "\t-m|--msbuild-path\tSet MSBuild.exe Path which inherently sets the version.\n\t\t\t\tExample: \"C:\Windows\Microsoft.NET\Framework64\\\v4.0.30319\MSBuild.exe\""
+	echo -e "\t-o|--outputdir\t\tSet Windows output path for the build. Defaults to ./bin/<release/debug>."
+	echo -e "\t--dont-stop-vm\t\tIf set, this will not kill the vm after the build"
+}
+
+function print_error {
+	if [[ $1 ]]; then
+		echo -e "[!] Error: $1"
+	fi
+}
+
+while [[ $# -gt 0 ]]; do
+	key="$1"
+
+	case $key in
+		-h|--help|--h)
+			print_help
+			exit
+			;;
+		-b|--buildir)
+			echo "$2"
+			if [[ "$2" =~ ^\. ]]; then
+				BUILD_DIR=$(readlink -f $2)
+				echo $BUILD_DIR
+			else
+				BUILD_DIR="$2"
+			fi
+			if [[ ! "$BUILD_DIR" =~ ^$Z_PATH ]]; then
+				print_help "The build path specified is not in the defined Z_PATH!"
+				exit
+			fi
+			shift
+			shift
+			;;
+		-r|--release)
+			if [[ $BUILD_DEBUG ]]; then
+				print_help "-r can't be used with -d!"
+				exit
+			fi
+			BUILD_RELEASE=1
+			BUILD_CONFIG="Release"
+			shift
+			;;
+		-d|--debug)
+			if [[ $BUILD_RELEASE ]]; then
+				print_help "-d can't be used with -r!"
+				exit
+			fi
+			BUILD_DEBUG=1
+			BUILD_CONFIG="Debug"
+			shift
+			;;
+		-o|--outputdir)
+			if [[ -z "$2" ]]; then
+				print_help "No output directory specified!"
+				exit
+			fi
+			TEMP="$2"
+			if [[ "${TEMP: -1}" !=  '\' ]]; then
+			       TEMP+='\'
+			fi		
+			OUTPUT_DIR=$TEMP
+			shift
+			shift
+			;;
+		-p|--platform)
+			if [[ "$2" != "x86" ]] && [[ "$2" != "x64" ]] && [[ "$2" != "AnyCpu" ]]; then
+				print_help "'$2' is not a legitimate platform option!"
+				exit
+			fi
+			BUILD_PLAT="$2"
+			shift
+			shift
+			;;
+		-m|--msbuild-path)
+			MSBUILD_PATH="$2"
+			shift
+			shift
+			;;
+		--dont-stop-vm)
+			DONT_STOP_VM=1
+			shift
+			;;
+	esac
+done
+
+if [[ -z $BUILD_DIR ]]; then
+	print_help "No build path specified!"
+	exit
+fi
+
+## Lets setup the defaults of the build isn't setup
+if [[ -z $BUILD_CONFIG ]]; then
+	echo "[-] Configuration not set using default ($DEF_BCONFIG)."
+	BUILD_CONFIG=$DEF_BCONFIG
+fi
+
+if [[ -z $BUILD_PLAT ]]; then
+	echo "[-] Platform not set using default ($DEF_BPLAT)."
+	BUILD_PLAT=$DEF_BPLAT
+fi
+
+if [[ -z $OUTPUT_DIR ]]; then
+	OUTPUT_DIR="$DEF_OUTPUTDIR$DEF_BPLAT/$BUILD_CONFIG/"
+	echo "[-] Output Directory not set, using default ($OUTPUT_DIR)."
+else
+	OUTPUT_DIR+=$DEF_BPLAT'\'$BUILD_CONFIG'\'
+fi
+
+if [[ -z $MSBUILD_PATH ]]; then
+	echo "[-] MSBuild path not set, using default ($DEF_MSBUILD)."
+	MSBUILD_PATH=$DEF_MSBUILD
+fi
+
+function check_vm_status {
+	echo "[+] Checking VM Status..."
+	VIRSH_LIST=$(virsh list | grep "$VIRSH_DOMAIN")
+	if [[ -z $VIRSH_LIST ]]; then
+		VM_STATUS=0
+	else
+		VM_STATUS=1
+	fi
+}
+
+function start_vm {
+	sudo virsh start "$VIRSH_DOMAIN"
+}
+
+function stop_vm {
+	sudo virsh shutdown "$VIRSH_DOMAIN" --mode acpi
+}
+
+## Check the vm - if it isn't started, start it.
+check_vm_status
+if [[ $VM_STATUS == 0 ]]; then
+	echo "[+] VM is not active. Starting..."
+	start_vm
+else
+	echo "[-] VM Already started. Skipping..."
+fi
+
+BUILD_FILE=$(echo "$BUILD_DIR" | rev | cut -d '/' -f 1 | rev)
+BUILD_CD=$(echo "$BUILD_DIR" | sed -E "s/$BUILD_FILE//g" | sed -E "s/\/opt/Z:/g" | sed -E "s/\//\\\/g" )
+BUILD_STRING=".'$MSBUILD_PATH' './$BUILD_FILE' /p:Configuration=$BUILD_CONFIG,OutputPath='$OUTPUT_DIR' /p:Platform='$BUILD_PLAT'"
+
+SSH_READY=0
+while [[ $SSH_RETRIES > 0 ]]; do
+	TEST_SSH=$(ssh -o ConnectTimeout=$SSH_TIMEOUT $SSH_NAME "exit")
+
+	if [[ $TEST_SSH ]]; then
+		SSH_READY=1
+		break
+	else
+		echo "[-] SSH not ready. Trying again in $SSH_TIMEOUT seconds..."
+		sleep 3
+		SSH_RETRIES=$(($SSH_RETRIES-1))
+	fi
+done
+
+if [[ $SSH_READY > 0 ]]; then
+	echo "[+] SSH Ready, Running build command ($BUILD_STRING) in $BUILD_CD"
+	ssh $SSH_NAME "cd '$BUILD_CD'; $BUILD_STRING"
+else
+	echo -e "[!] SSH did not initialize!"
+fi
+
+if [[ -z $DONT_STOP_VM ]]; then
+	echo "[+] Shutting down VM..."
+	stop_vm
+else
+	echo "[-] DONT_STOP_VM set. Skipping shutdown."
+fi
+
